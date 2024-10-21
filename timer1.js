0c0799996be120bc1c3aab2db9d6f920f3e1e739
Nik Hixson <nikilobe@gmail.com>
2024-10-20 21:18:13 -0700

    filled in my code and my comments describe it. Needed to use google for isNaN

--- timer1.js
+++ timer1.js
@@ -2,9 +2,17 @@
 const alarm = process.argv.slice(2);
 
 //loop to go through process.argv
+for (let time of alarm) {
+  let beep = Number(time);
 
-//if no number then no beep and end.
+  //if no number then no beep and end.
+  //else if number is not a number |or| number is negative ignore/skip but
+  if (beep < 0 || isNaN(beep)) {
+    continue;
+  }
 
-//else if number is not a number |or| number is negative ignore/skip but
-
-//setTimout to handle whatever number is [i] * 1000(1 second)
+  //setTimout to handle whatever number is [i] * 1000(1 second)
+  setTimeout(() => {
+    process.stdout.write("\x07");
+  }, beep * 1000);
+}
