7fac73a9f432473bced655150abf97e4e7327c71
Nik Hixson <nikilobe@gmail.com>
2024-10-20 21:09:05 -0700

    wrote out my pseudocode and started with process.argv.slice(2);

--- timer1.js
+++ timer1.js
@@ -1,5 +1,10 @@
+//Using a variable to replace process.argv and slicing it so that it will start the loop at index 2 which is where the first number argument will be.
+const alarm = process.argv.slice(2);
 
+//loop to go through process.argv
 
+//if no number then no beep and end.
 
+//else if number is not a number |or| number is negative ignore/skip but
 
-process.stdout.write("\x07");
+//setTimout to handle whatever number is [i] * 1000(1 second)
